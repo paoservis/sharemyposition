c22501a9f66853b80b460d844ca27aa09d28e01f
smaucourt <smaucourt@6b4730c2-fbba-11de-96e6-c767aa2c8147>
2012-01-14 13:03:04 +0000

    zoom plus elevé, fin de la beta

--- ShareMyPosition-android/src/net/sylvek/sharemyposition/ShareMyPosition.java
+++ ShareMyPosition-android/src/net/sylvek/sharemyposition/ShareMyPosition.java
@@ -87,9 +87,9 @@ public class ShareMyPosition extends MapActivity implements LocationListener {
 
     public static final String LOG = "ShareMyPosition";
 
-    private static final String VERSION = "1.1.0-beta4";
+    private static final String VERSION = "1.1.0";
 
-    private static final int ZOOM_LEVEL = 16;
+    private static final int ZOOM_LEVEL = 17;
 
     private static final String HOST = "http://sharemyposition.appspot.com/";
 
